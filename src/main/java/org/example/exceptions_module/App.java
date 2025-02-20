db880b5fbe2a8e1ed13bacef595ab2f6b4dee073
user <user@10.78.30.14>
2025-02-20 12:30:20 +0300

    first commit

---
+++ src/main/java/org/example/exceptions_module/App.java
@@ -0,0 +1,13 @@
+package org.example.exceptions_module;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
